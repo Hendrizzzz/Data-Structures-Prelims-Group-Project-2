e23682ab9349f6b7958dbd48b412a19388420363
HYOWON ARZIL BERNABE <2244644@slu.edu.ph>
2024-09-16 22:00:09 +0800

    update reference classes

--- src/prelim/File.java
+++ src/prelim/File.java
@@ -1,4 +1,57 @@
 package prelim;
 
-public class File {
+public class File<T> implements Comparable<T> {
+    private String fileName;
+    private String extension;
+    private int size;
+
+    public File() {
+        this.fileName = "";
+        this.extension = "";
+        this.size = 0;
+    }
+
+    public File(String fileName, String extension, int size) {
+        this.fileName = fileName;
+        this.extension = extension;
+        this.size = size;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "fileName='" + fileName + '\'' +
+                ", extension='" + extension + '\'' +
+                ", size=" + size +
+                '}';
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return 0;
+    }
 }
--- src/prelim/Folder.java
+++ src/prelim/Folder.java
@@ -1,4 +1,46 @@
 package prelim;
 
-public class Folder {
+public class Folder<T> implements Comparable<T> {
+    private String folderName;
+    private LinkedList<File> files;
+
+    public Folder() {
+        this.folderName = "";
+        this.files = null;
+    }
+
+    public Folder(String folderName) {
+        this.folderName = folderName;
+        this.files = null;
+    }
+
+    public String getFolderName() {
+        return folderName;
+    }
+
+    public LinkedList<File> getFiles() {
+        return files;
+    }
+
+    public void setFolderName(String folderName) {
+        this.folderName = folderName;
+    }
+
+    public void setFiles(LinkedList<File> files) {
+        this.files = files;
+    }
+
+    @Override
+    public String toString() {
+        return "Folder{" +
+                "folderName='" + folderName + '\'' +
+                ", files=" + files +
+                '}';
+    }
+
+
+    @Override
+    public int compareTo(T o) {
+        return 0; // Fix later
+    }
 }
