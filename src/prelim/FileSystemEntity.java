9ca2c16570ecfdf2e00c4405989d29c8ed0f9421
JIM HENDRIX BAG-EO <2243493@slu.edu.ph>
2024-10-04 06:18:46 +0800

    Add a Superclass for File and Folder class

---
+++ src/prelim/FileSystemEntity.java
@@ -0,0 +1,73 @@
+package prelim;
+
+import java.util.Date;
+
+public abstract class FileSystemEntity {
+    private String name;
+    private String path;
+    private int size; // or double?
+    private Date creationDate;
+    private Date modificationDate;
+
+    public FileSystemEntity() {
+
+    }
+
+    public FileSystemEntity(String name, Date creationDate) {
+        this.name = name;
+        this.creationDate = creationDate;
+    }
+
+    public FileSystemEntity(String name, String path, Date creationDate) {
+        this.name = name;
+        this.path = path;
+        this.creationDate = creationDate;
+    }
+
+
+    @Override
+    public abstract String toString();
+
+    @Override
+    public abstract boolean equals(Object e);
+
+    @Override
+    public abstract int hashCode();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    protected void setSize(int size) {
+        this.size = size;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    protected void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
