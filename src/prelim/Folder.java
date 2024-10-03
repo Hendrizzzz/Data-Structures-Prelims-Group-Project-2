719bf671fffe6accba869d9e407d907faacea81f
JIM HENDRIX BAG-EO <2243493@slu.edu.ph>
2024-10-04 06:21:40 +0800

    Implemented the superclass / abstract class FileSystemEntity

--- src/prelim/Folder.java
+++ src/prelim/Folder.java
@@ -1,84 +1,143 @@
 package prelim;
 
+import java.util.Date;
+import java.util.NoSuchElementException;
 import java.util.Objects;
-/**
- * TODO
- * */
-public class Folder implements Comparable<Folder> {
-    private String folderName;
-    private LinkedList<Object> contents;
-    /**
-     * Default constructor for the Folder class.
-     * */
+
+
+public class Folder extends FileSystemEntity implements Comparable<Folder> {
+    private LinkedList<FileSystemEntity> subContents;
+
+
     public Folder() {
-        this.folderName = "";
-        this.contents = null;
-    }
-    /**
-     * Constructor for an empty Folder.
-     *
-     * @param folderName The name of the folder to be constructed.
-     * */
-    public Folder(String folderName) {
-        this.folderName = folderName;
-        this.contents = null;
-    }
-    /**
-     * @return The name of a folder.
-     * */
-    public String getFolderName() {
-        return folderName;
-    }
-    /**
-     * @return The contents of a folder.
-     * */
-    public LinkedList<Object> getContents() {
-        return contents;
-    }
-    /**
-     * @return True/ False if two folders are the same.
-     * */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        Folder folder = (Folder) o;
+    }
 
-        return Objects.equals(folderName, folder.folderName);
+    public Folder(String name) {
+        this.setName(name);
     }
+
+    public Folder(String name, Date creationDate) {
+        super (name, creationDate);
+    }
+
+
+    public Folder(String name, LinkedList<FileSystemEntity> subContents, Date creationDate) {
+        super(name, creationDate);
+        this.subContents = subContents;
+
+        // Get the sum of all the files' sizes
+        int size = 0;
+        for (int i = 0; i < subContents.getSize(); i++)
+            size += subContents.getElement(i).getSize();
+
+        this.setSize(size);
+    }
+
+
+
+    public LinkedList<FileSystemEntity> getSubContents() {
+        return subContents;
+    }
+
+
     /**
-     * Generates a hash code for a folder.
-     * @return An integer to represent the hash code.
-     * */
-    @Override
-    public int hashCode() {
-        return folderName != null ? folderName.hashCode() : 0;
+     * Adds a file to this Folder
+     * @param file the file to be added in this Folder.
+     */
+    public void addFile(File file) {
+        addFileSystemEntity(file);
     }
+
     /**
-     * Sets the name for a folder.
-     * @param folderName The specified name of a folder.
-     * */
-    public void setFolderName(String folderName) {
-        this.folderName = folderName;
+     * Adds a folder to this Folder
+     * @param folder the folder to be added in this Folder.
+     */
+    public void addFolder(Folder folder) {
+        addFileSystemEntity(folder);
     }
+
+    private void addFileSystemEntity(FileSystemEntity newEntity) { // modify if the file has a datafield of contents too
+        // verify if the name of the newFile doesn't exist in the folder yet
+        int fileNameAppearance = 0;
+        for (int i = 0; i < subContents.getSize(); i++) {
+            if (isSameEntity(subContents.getElement(i), newEntity))
+                fileNameAppearance++;
+        }
+
+        // Update the fileName if there exists a fileName already
+        if (fileNameAppearance != 0)
+            newEntity.setName(getNameWithDuplicateIdentifier(newEntity, fileNameAppearance)); // repeatedName4Times (4).pdf
+
+        subContents.insert(newEntity);
+    }
+
+    private boolean isSameEntity(FileSystemEntity entity, FileSystemEntity newEntity) {
+        return entity.equals(newEntity);
+    }
+
+    private String getNameWithDuplicateIdentifier(FileSystemEntity newFile, int fileNameAppearance) {
+        return newFile.getName() + " (" + fileNameAppearance + ")";
+    }
+
+
     /**
-     * Set the contents within a folder.
-     * @param contents The content to be placed within a folder.
-     * */
-    public void setContents(LinkedList<Object> contents) {
-        this.contents = contents;
+     * Deletes a file in this Folder
+     * @param file the file to be deleted.
+     * @throws ListEmptyException when this Folder is empty.
+     */
+    public void removeFile(File file) throws ListEmptyException {
+        removeFileSystemEntity(file);
     }
+
     /**
-     * @return A string representation for a folder.
-     * */
-    @Override
-    public String toString() {
-        return "Folder{" +
-                "folderName='" + folderName + '\'' +
-                ", contents=" + contents +
-                '}';
+     * Deletes a Folder in this Folder.
+     * @param folder the folder to be deleted.
+     * @throws ListEmptyException when this Folder is empty.
+     */
+    public void removeFolder(Folder folder) throws ListEmptyException {
+        removeFileSystemEntity(folder);
     }
+
+    private void removeFileSystemEntity(FileSystemEntity fileSystemEntity) throws ListEmptyException {
+        if (subContents.getSize() == 0)
+            throw new ListEmptyException("The folder is empty. ");
+
+        for (int i = 0; i < subContents.getSize(); i++)
+            if (isSameEntity(subContents.getElement(i), fileSystemEntity)) {
+                subContents.deleteAtIndex(i);
+                return; // stop the traversing now
+            }
+
+        throw new NoSuchElementException("File / Folder not found. Cannot delete. ");
+    }
+
+
+    public void renameAFileInsideThisFolder(File oldFile, String name, String extension) {
+        for (int i = 0; i < subContents.getSize(); i++) {
+            FileSystemEntity currentEntity = subContents.getElement(i);
+            if (isSameEntity(currentEntity, oldFile)) {
+                File fileToUpdate = (File) currentEntity;
+                fileToUpdate.setName(name);
+                fileToUpdate.setExtension(extension);
+                return;
+            }
+        }
+    }
+
+    public void renameAFolderInsideThisFolder(Folder oldFolder, String newName) {
+        for (int i = 0; i < subContents.getSize(); i++) {
+            FileSystemEntity currentEntity = subContents.getElement(i);
+            if (isSameEntity(currentEntity, oldFolder)) {
+                Folder folderToUpdate = (Folder) currentEntity;
+                folderToUpdate.setName(newName);
+                return;
+            }
+        }
+    }
+
+
+
     /**
      * Compares two folders
      *
@@ -89,4 +148,35 @@ public class Folder implements Comparable<Folder> {
     public int compareTo(Folder o) {
         return 0; // Fix later
     }
+
+    /**
+     * Compares this folder to the other (o) folder by name.
+     * This will be used to identify when the folder name needs a duplicate identifier when adding a folder.
+     * Ex: DupFolder -> DupFolder (2)
+     * @param o the other Folder
+     * @return true if two objects have the same name, or basically the same reference.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Folder folder = (Folder) o;
+
+        return Objects.equals(getName(), folder.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return getName() != null ? Objects.hash(getName()) : 0;
+    }
+
+    /**
+     * @return String representation of this Folder.
+     */
+    @Override
+    public String toString() {
+        return getName();
+    }
+
 }
