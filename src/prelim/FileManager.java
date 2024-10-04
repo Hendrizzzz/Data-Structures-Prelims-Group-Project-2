21feff53e0a67956ae305303acc0a7aacc34734a
aaron miguel cardenas <2242828@slu.edu.ph>
2024-10-04 19:28:10 +0800

    Added javadoc to FileManager.

--- src/prelim/FileManager.java
+++ src/prelim/FileManager.java
@@ -1,13 +1,21 @@
 package prelim;
-
+/**
+ * TODO
+ * */
 public class FileManager {
     private Folder rootFolder;
-
+    /**
+     * Default constructor for the {@code FileManager} class.
+     * */
     public FileManager() throws ListOverflowException {
         rootFolder = new Folder("root");
         initializeDefaultFolders();
     }
-
+    /**
+     * Initializes a {@code Folder} for each specific directories:
+     * <p>
+     * Desktop, Documents, Pictures, Music and Videos
+     * */
     private void initializeDefaultFolders() throws ListOverflowException {
         String[] defaultFolders = {"Desktop", "Documents", "Pictures", "Music", "Videos"};
         for (String folderName : defaultFolders) {
@@ -15,11 +23,15 @@ public class FileManager {
             rootFolder.addSubfolder(newFolder);
         }
     }
-
+    /**
+     * @return The root folder from a {@code Folder}.
+     * */
     public Folder getRootFolder() {
         return rootFolder;
     }
-
+    /**
+     * TODO
+     * */
     public Folder getDefaultFolder(String folderName) {
         for (Folder folder : rootFolder.getSubfolders()) {
             if (folder.getFolderName().equalsIgnoreCase(folderName)) {
@@ -28,7 +40,9 @@ public class FileManager {
         }
         return null; // Folder not found
     }
-
+    /**
+     * TODO
+     * */
     public void displayDefaultFolders() {
         System.out.println("Default Folders:");
         for (Folder folder : rootFolder.getSubfolders()) {
